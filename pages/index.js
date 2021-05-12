f5a27ac0922f90a51c14cf34ab937eba05e83841
kinson <swilkinsonhunter@gmail.com>
2021-05-11 22:56:40 -0600

    add next / styled-components boilerplate and add mo' links

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,8 @@ import styled from 'styled-components';
 const MastGroupStyles = styled.div`
   width: 60rem;
   position: relative;
-  transform: rotate(-35deg);
-  left: -10rem;
+  transform: rotate(-43deg);
+  left: -14rem;
 
   @media (max-width: 45rem) {
     transform: rotate(-44deg);
@@ -40,7 +40,7 @@ const LinksBoxStyles = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
-  margin: 0.25rem 0;
+  margin-bottom: 1rem;
 `;
 
 const LinkStyles = styled.a`
@@ -48,6 +48,7 @@ const LinkStyles = styled.a`
   font-family: 'Libre Baskerville', serif;
   margin: 0 0.55rem;
   color: #0069d9;
+  letter-spacing: 1px;
 
   :hover {
     text-decoration: underline;
@@ -57,10 +58,22 @@ const LinkStyles = styled.a`
   :first-child {
     margin-left: 0;
   }
+
+  :last-child {
+    margin-right: 0;
+  }
 `;
 
 const PageStyles = styled.div`
   overflow: hidden;
+`;
+
+const Subtitle = styled.p`
+  font-size: 97%;
+  font-family: 'Lato', sans-serif;
+  color: #222;
+  letter-spacing: 1px;
+  margin: 0.75rem 0;
 `;
 
 export default function Home() {
@@ -70,7 +83,7 @@ export default function Home() {
         <title>Sam Hunter</title>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Libre+Baskerville&family=Squada+One&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Libre+Baskerville&family=Squada+One&family=Lato:wght@300&display=swap"
           rel="stylesheet"
         />
       </Head>
@@ -83,12 +96,21 @@ export default function Home() {
 
       <MiddleNameStyles>
         <BigNameStyles>Welcome 👋🏼</BigNameStyles>
+        <Subtitle>Where You Can Find Me:</Subtitle>
         <LinksBoxStyles>
           <LinkStyles href="https://github.com/kinson/">Github</LinkStyles>
           <LinkStyles href="https://www.linkedin.com/in/samwhunter/">
             LinkedIn
           </LinkStyles>
+          <LinkStyles href="https://twitter.com/_huntersw">Twitter</LinkStyles>
+        </LinksBoxStyles>
+        <Subtitle>What I'm Working On:</Subtitle>
+        <LinksBoxStyles>
+          <LinkStyles href="https:/goexponent.com/">Exponent</LinkStyles>
           <LinkStyles href="https://tunes.samwhunter.com/">Tunes</LinkStyles>
+          <LinkStyles href="https://bigbeeftracker.com">
+            Big Beef Tracker
+          </LinkStyles>
         </LinksBoxStyles>
       </MiddleNameStyles>
     </PageStyles>
